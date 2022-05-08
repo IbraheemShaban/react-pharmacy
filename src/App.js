a40ad498ec7ed4b710070204831c99c0b640e93c
IbraheemShaban <ishaban097@gmail.com>
2022-05-08 20:09:53 +0300

    FINISHED tasks 1,2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { products } from './products.js';
+const productsList = products;
 
 function App() {
+  const result = productsList.map((e) => (
+    <div className="product" key={e.id}>
+      <img className="product-img" src={e.img} />
+      <p>{e.name}</p>
+      <p>{e.price} kd</p>
+    </div>
+  ));
+  console.log(result);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Boots Pharmacy</h1>
+      <h4>Where you buy medicine</h4>
+      <img
+        className="image"
+        src="https://media.istockphoto.com/photos/boots-local-pharmacy-facade-picture-id458633357?k=20&m=458633357&s=612x612&w=0&h=eXn-tv4-FIz6NNVu3Iw0zhIyOZK78ipCxPlxWROYplg="
+        alt="pharmacy"
+      />
+      <div className="products-list"> {result}</div>
     </div>
   );
 }
